456daf4eebac9863b5827e6a100b62f651be2ae4
Samuel Oyewole <findseunoyewole@gmail.com>
2022-01-10 03:35:19 -0800

    dashboard light $dark mode

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+
+
+const toggleIcon = document.querySelector('.toggle-icon');
+header = document.querySelector('.header')
+
+
+
+toggleIcon.addEventListener('click', () => {
+
+    // if(header.classList.contains('header')){
+    //     header.classList.remove('header')
+    // }
+    // else{
+    //     header.classList.add('header')
+    // }
+    
+})
